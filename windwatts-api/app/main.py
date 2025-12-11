fcfa1c840735d5935a3127568ce17296ef9115ab
Roy Li <RLiNREL@users.noreply.github.com>
2025-12-10 17:09:28 -0700

    add new controller to main app

--- windwatts-api/app/main.py
+++ windwatts-api/app/main.py
@@ -7,6 +7,7 @@ from fastapi.responses import JSONResponse
 from mangum import Mangum
 from app.controllers.wtk_data_controller import router as wtk_data_router
 from app.controllers.era5_data_controller import router as era5_data_router
+from app.controllers.wind_data_controller import router as wind_data_router
 from app.middleware import AuditMiddleware, LoggingMiddleware
 from app.exception_handlers import log_unhandled_exceptions, log_validation_errors
 from textwrap import dedent
@@ -50,8 +51,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(wtk_data_router, prefix="/wtk", tags=["wtk-data"])
-app.include_router(era5_data_router, prefix="/era5", tags=["era5-data"])
+# API v1
+app.include_router(wind_data_router, prefix="/v1", tags=["v1-wind-data"])
+
+# Legacy routes - Deprecated
+# TODO: Remove these routes
+app.include_router(
+    wtk_data_router, 
+    prefix="/wtk", 
+    tags=["wtk-data (deprecated)"],
+    deprecated=True
+)
+app.include_router(
+    era5_data_router, 
+    prefix="/era5", 
+    tags=["era5-data (deprecated)"],
+    deprecated=True
+)
 
 @app.get("/healthcheck", response_model=HealthCheckResponse)
 def healthcheck():
