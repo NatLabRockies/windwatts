89e3a8c88cd10733fd601b7a737276828432378d
Roy Li <RLiNREL@users.noreply.github.com>
2026-03-30 11:25:39 -0600

    add windrose bearing constant

---
+++ windwatts-ui/src/constants/windRose.ts
@@ -0,0 +1,31 @@
+export const WIND_ROSE_DIRECTIONS_16 = [
+  "N",
+  "NNE",
+  "NE",
+  "ENE",
+  "E",
+  "ESE",
+  "SE",
+  "SSE",
+  "S",
+  "SSW",
+  "SW",
+  "WSW",
+  "W",
+  "WNW",
+  "NW",
+  "NNW",
+] as const;
+
+export const WIND_ROSE_DIRECTIONS_8 = [
+  "N",
+  "NE",
+  "E",
+  "SE",
+  "S",
+  "SW",
+  "W",
+  "NW",
+] as const;
+
+export const WIND_ROSE_DIRECTIONS_4 = ["N", "E", "S", "W"] as const;
